26dc33cf692ad5c5aa92c4f79eeb32f9c93f4264
FATIIMA29 <160587289+FATIIMA29@users.noreply.github.com>
2024-11-10 08:17:21 -0800

    Removed unused import from contact page & page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import '@/style/contact.css'
-import Link from 'next/link'
 const page = () => {
   return (
     <div >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-"use client"
+
 import Hero from '@/components/hero'
 import Skills from '@/app/skills/page'
-import React, {useEffect} from 'react'
+
 import About from '@/app/about/page'
 import Project from '@/app/projects/page'
 import Contact from '@/app/contact/page'
-import Aos from 'aos';
+
 export default function Home() {
-  useEffect(()=>{Aos.init({
-    easing:'ease-out-back',
-    duration:1200,
-    delay:100,
-    mirror:true,
-    anchorPlacement:"bottom-bottom",
-    offset:160, 
-  });
-  Aos.refresh()
-},[]);
+ 
 
   return (
 
